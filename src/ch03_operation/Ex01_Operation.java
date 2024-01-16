776172ea93a686d994cdb2a76ced21917fd6cee0
Koodohyeon <koodh6523@naver.com>
2024-01-16 10:42:13 +0900

    1커밋

---
+++ src/ch03_operation/Ex01_Operation.java
@@ -0,0 +1,12 @@
+package ch03_operation;
+
+public class Ex01_Operation {
+
+	public static void main(String[] args) {
+		int i = 2, k = 3;
+		double f = k / (double) i;
+		System.out.printf("%f, %d, %d%n", (double)k/i, i++, --k); // 1.5, 2, 2
+
+	}
+
+}
